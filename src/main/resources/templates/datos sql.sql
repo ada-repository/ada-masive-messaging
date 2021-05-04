5f925a53d573c9343da1e268c0a7f7cfa9e58f81
xiangn <xiangnrodriguz@gmail.com>
2021-05-03 20:07:24 -0400

    Controlador Plans Funcionando

--- src/main/resources/templates/datos sql.sql
+++ src/main/resources/templates/datos sql.sql
@@ -1,3 +1,6 @@
 insert into users values (1,'29/4/2021','j-2145','probando@gmail.com','juan','123421312',true)
 
 insert into messages values (4,'1-5-2021','Fiorela Te amo','Fiorela','+584264881229','Enviado','Prueba 3',1)
+
+
+insert into plans values (1,'BASICO','ELLOS','30',1)
