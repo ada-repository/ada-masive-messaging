3bbcb2fe7d603dbe37ca5045b547b70cd0610ae7
IangnX <xiangnrodriguez@gmail.com>
2021-05-16 17:15:24 -0400

    Validaciones de sendSMS en espera de aprobacion. Por mi parte estan al 100%. Guarda los sms enviados en la BD

--- src/main/java/com/fioxin/messaging/messaging/Domain/Service/Implements/MessageServiceImpl.java
+++ src/main/java/com/fioxin/messaging/messaging/Domain/Service/Implements/MessageServiceImpl.java
@@ -9,12 +9,13 @@ import com.fioxin.messaging.messaging.Domain.Entity.User;
 import com.fioxin.messaging.messaging.Domain.Repository.MessageJpaRepository;
 import com.fioxin.messaging.messaging.Domain.Service.IMessageService;
 import com.fioxin.messaging.messaging.Domain.Service.IUserService;
+import com.fioxin.messaging.messaging.domain.Service.IPlanService;
 import com.fioxin.messaging.messaging.domain.entity.Subscription;
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,13 +26,17 @@ import org.springframework.stereotype.Service;
 public class MessageServiceImpl implements IMessageService{
 
     public static final String ACCOUNT_SID = "ACa46c31f03e3a56eb1fe96d771c4e8dcb";
-    public static final String AUTH_TOKEN = "6183e6ed35b4e8f1b1d39e7fc3cea749";
+    public static final String AUTH_TOKEN = "972dabfef8df7547468d442505c5bf0a";
 
     @Autowired
     private MessageJpaRepository messageRepo;
 
     @Autowired
     private IUserService userService;
+    
+    @Autowired
+    private IPlanService planService;
+    
 
     @Override
     public List<NotificationMessage> getAllMessages() {
@@ -82,17 +87,15 @@ public class MessageServiceImpl implements IMessageService{
             response.put("Mensaje", "Usuario no existe en la Base de datos");
             return response;
         }
-        //Buscamos la subscripcion del usuario que este activa. 
-      Subscription subscrip =  user.getSubscription().
-                stream().
-                filter(subs -> subs.getStatus().equals("Activo")).findFirst().orElse(null);
+        //Validamos la Subscripcion del usuariom (que sea sms, este activa y este dentro de la fecha)
+    boolean rpta = user.getSubscription().stream()
+            .anyMatch( s -> isSMS(s));           
       
-      //Validamos que la fecha de la subscripcion este activa
-       if (!(LocalDate.parse(subscrip.getEndDate().toString()).isBefore(LocalDate.parse(LocalDate.now().toString())))){
-          response.put("Mensaje", "Subscripcion expirada. Por favor Renuevela");
+    if(!rpta ){
+            response.put("Mensaje", "El usuario no tiene subscripcion desponible para esta opcion ");
             return response;
-       }
-       
+    }
+   
        //Validamos el tamaño del mensaje
     if( finalMessage.length() > 160){
           response.put("Mensaje", "El tamaño del mensaje excede los 160 caracteres.");
@@ -100,32 +103,44 @@ public class MessageServiceImpl implements IMessageService{
     }
     
         SendMessageRequest sendMessage = new SendMessageRequest();
-        NotificationMessage notification = new NotificationMessage(); 
-        List<NotificationMessage> listNoti = new ArrayList<>();     
+        
+        List<NotificationMessage> listNoti = new LinkedList<>();     
         //Comenzamos a enviar los mensajes YUPIII!!
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);     
     //    sendMessage.setUser(user); //Lo retornaremos en la respuesta de la api
+    
+    
        messages.getMessages().forEach( (sms) -> {
                Message message = Message.creator(                    
                new com.twilio.type.PhoneNumber(sms.getReceiverNumber()), //to
                new com.twilio.type.PhoneNumber("+12057076733"),      //from          
                finalMessage)
                .create();
+              NotificationMessage notification = new NotificationMessage();           
                notification.setReceiverNumber(sms.getReceiverNumber());
                notification.setMessage(finalMessage);
                notification.setStatus(message.getStatus().toString());
                notification.setNameReceiver(sms.getNameReceiver());
                notification.setCreatedAt(Date.from(message.getDateCreated().toInstant()));
                notification.setSubject(sms.getSubject());
-               notification.setUserId(user.getId());
+               notification.setUserId(user.getId());  
                listNoti.add(notification); 
              }                         
        );
+       
       sendMessage.setIdUser(user.getId());
       sendMessage.setMessage(finalMessage);            
       sendMessage.setMessages(listNoti);
+      messageRepo.saveAll(listNoti);
+      response.put("Mensajes", "Mensajes Enviados: "+listNoti.size());
       response.put("Mensajes", sendMessage);
       return response;
+    }
+    
+    
+    private boolean isSMS(Subscription s){
+        return "SMS".equals(s.getPlan().getCategory().getName()) && s.getStatus() == true && !(LocalDate.parse(s.getEndDate().toString()).isBefore(LocalDate.parse(LocalDate.now().toString()))) ;
+       
     }
 }
 /*  System.out.println("SID: "+message.getSid());
