ae2c9c3737f8f15c88a5803d606b1d7596c9d23e
IangnX <xiangnrodriguez@gmail.com>
2021-09-03 16:17:58 -0400

    Corrigiendo ruta home

--- src/main/java/com/ada/system/mass/messaging/controller/HomeController.java
+++ src/main/java/com/ada/system/mass/messaging/controller/HomeController.java
@@ -11,6 +11,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,9 +25,9 @@ public class HomeController {
     
     @Autowired
     private IHomeService home;
-
+    
+    @RequestMapping("/")
     @ResponseStatus(HttpStatus.OK)
-    @GetMapping("/")
     public List<String> home(){
          return  home.infoCompany();     
     }    
